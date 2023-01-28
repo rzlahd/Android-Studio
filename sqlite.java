f584d432e54a85e3b95387626ec339da36bfae39
izal <58200011+rzlahd@users.noreply.github.com>
2023-01-28 01:10:36 -0800

    Create sqlite.java

---
+++ sqlite.java
@@ -0,0 +1,29 @@
+
+public class DbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+    private static final String SQL_CREATE_ENTRIES =
+        "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+        FeedEntry._ID + " INTEGER PRIMARY KEY," +
+        FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+        FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+    private static final String SQL_DELETE_ENTRIES =
+    "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
